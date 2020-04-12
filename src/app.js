8cd9ccd715042583a484f4c5d553c6fc5faf435c
quijano <safiro.quijano@nokia.com>
2020-04-12 23:42:25 +0800

    added filtering of covid 19 info results based on geolocation

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const port = process.env.PORT || 3000;
 const {getNearbyX} = require('./datasrc/covidinfo');
 const {getGeoLocation} = require('./geolocation/geolocation');
 
+
 app.get('/',(req,res)=>{
     res.send('hello world');
 });
@@ -11,22 +12,28 @@ app.get('/',(req,res)=>{
 //this will be used for getting hospitals near queried coordinates
 app.get('/hospitals',(req,res)=>{
     if(!req.query.address) return res.send({error: 'no address provided!'});
-
-    options = {lat:0, long:0, type:"hospitals"};
-    getNearbyX(options,(error,data)=>{
+    getGeoLocation(req.query.address, (error,geodata)=>{
         if(error) return res.send({error});
-        res.send(data);
+        inputData = {geodata, type:"hospitals"};
+
+        getNearbyX(inputData,(error,data)=>{
+            if(error) return res.send({error});
+            res.send({geodata,data});
+        });
     });
 });
 
 //this will be used for getting individuals near provided coordinates
 app.get('/individuals',(req,res)=>{
     if(!req.query.address) return res.send({error: 'no address provided!'});
-    
-    options = {lat:0, long:0, type:"individuals"}
-    getNearbyX(options,(error,data)=>{
+    getGeoLocation(req.query.address, (error,geodata)=>{
         if(error) return res.send({error});
-        res.send(data);
+        inputData = {geodata, type:"individuals"};
+
+        getNearbyX(inputData,(error,data)=>{
+            if(error) return res.send({error});
+            res.send({geodata,data});
+        });
     });
 });
 
--- src/datasrc/covidinfo.js
+++ src/datasrc/covidinfo.js
@@ -1,12 +1,35 @@
 const axios = require('axios');
 const covidurl = 'https://coronavirus-ph-api.herokuapp.com'
 
-const getNearbyX = (options,callback) =>{
-    const typeMap = {individuals:"cases", hospitals:"facilities"};
-    const response = axios.get(`${covidurl}/${typeMap[options.type]}`);
-    
+//bbox contains the information about the land area by providing 2 sets of coordinates
+const getLandArea = (geodata) =>{
+    //create an arbitrary landarea if bbox is not available
+    if(!geodata.bbox) {
+        return [geodata.coordinates[0]-.01, //longitude
+                geodata.coordinates[1]-.01, //latitude
+                geodata.coordinates[0]+.01, //longitude, and then another latitude
+                geodata.coordinates[1]+.01].map((curr)=>Number(curr.toPrecision(9)));
+    }
+    return geodata.bbox;
+}
+
+//nope, not that X
+const checkIfXisWithinTheArea = (landAreaCoordinates, currLongitude, currLatitude) => {
+    const longitudeRange = [landAreaCoordinates[0],landAreaCoordinates[2], currLongitude].sort();
+    const latitudeRange = [landAreaCoordinates[1], landAreaCoordinates[3], currLatitude].sort();
+    return (longitudeRange.indexOf(currLongitude) === 1) && (latitudeRange.indexOf(currLatitude) === 1) ? true : false;
+};
+
+const getNearbyX = (inputData,callback) =>{
+    const searchType = {individuals:"cases", hospitals:"facilities"};
+    const landAreaCoordinates = getLandArea(inputData.geodata);
+    const response = axios.get(`${covidurl}/${searchType[inputData.type]}`);
+
     response.then((response)=>{
-        callback(undefined,response.data);
+        const elementsWithinTheArea = response.data.filter((curr) => {
+            return checkIfXisWithinTheArea(landAreaCoordinates,curr.longitude,curr.latitude);
+        });
+        callback(undefined,elementsWithinTheArea);
     }).catch((error)=>{
         callback(error,undefined);
     });
