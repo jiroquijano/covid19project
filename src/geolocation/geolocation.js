91fec64186fdfe9ae394a445fc4f6a0cad622bb3
quijano <safiro.quijano@nokia.com>
2020-04-12 19:40:07 +0800

    edited return data for geolocation

--- src/geolocation/geolocation.js
+++ src/geolocation/geolocation.js
@@ -1,12 +1,20 @@
 const axios = require('axios');
 
-const getGeoLocation = (address) =>{
+const getGeoLocation = (address,callback) =>{
     const mapboxURL = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(address)}.json?limit=3&access_token=pk.eyJ1Ijoiamlyb3F1aWphbm8iLCJhIjoiY2s4MXJvbW9xMHRwNzNtb3E4djltN3UxaCJ9.XJy-r35twz2mNNXikiALHA`;
     const mapboxResult = axios(mapboxURL);
     mapboxResult.then((response)=>{
-        console.log(response.data.features[0]);
+        const {place_name:name, bbox, center:coordinates} = response.data.features[0];
+        const alternativePlaces = [response.data.features[1].text, response.data.features[2].text];
+        console.log(similarPlaces);
+        callback(undefined,{
+            name,
+            bbox,
+            coordinates,
+            alternativePlaces
+        });
     }).catch((error)=>{
-        console.log(error);
+        callback(error,undefined);
     });
 }
 
