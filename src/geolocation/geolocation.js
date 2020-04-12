c6fe161c6b6cc8ce0f03e2c2b54cc87f90addb6e
quijano <safiro.quijano@nokia.com>
2020-04-12 21:23:53 +0800

    localized results to philippines only

--- src/geolocation/geolocation.js
+++ src/geolocation/geolocation.js
@@ -1,12 +1,19 @@
 const axios = require('axios');
 
+const localizeToPhilippinesOnly = (places) =>{
+    return places.filter((curr)=>{
+        return curr.place_name.toLowerCase().includes("philippines");
+    });
+}
+
 const getGeoLocation = (address,callback) =>{
-    const mapboxURL = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(address)}.json?limit=3&access_token=pk.eyJ1Ijoiamlyb3F1aWphbm8iLCJhIjoiY2s4MXJvbW9xMHRwNzNtb3E4djltN3UxaCJ9.XJy-r35twz2mNNXikiALHA`;
+    const mapboxURL = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(address)}.json?limit=5&access_token=pk.eyJ1Ijoiamlyb3F1aWphbm8iLCJhIjoiY2s4MXJvbW9xMHRwNzNtb3E4djltN3UxaCJ9.XJy-r35twz2mNNXikiALHA`;
     const mapboxResult = axios(mapboxURL);
     mapboxResult.then((response)=>{
-        const {place_name:name, bbox, center:coordinates} = response.data.features[0];
-        const alternativePlaces = [response.data.features[1].text, response.data.features[2].text];
-        console.log(similarPlaces);
+        const localizedPlaces = localizeToPhilippinesOnly(response.data.features);
+        const mostRelevant = localizedPlaces[0];
+        const {place_name:name, bbox, center:coordinates} = mostRelevant;
+        const alternativePlaces = localizedPlaces.slice(1).map((curr)=>curr.place_name); //suggestions of other places
         callback(undefined,{
             name,
             bbox,
