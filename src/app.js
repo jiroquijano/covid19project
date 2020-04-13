556d51daa62e11ad7f111abbe8ca27259513d781
quijano <safiro.quijano@nokia.com>
2020-04-13 19:11:26 +0800

    made the frontend pretty

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const hbs = require('hbs');
 const publicDirectoryPath = path.join(__dirname,'../public');
 const viewPath = path.join(__dirname,'../templates/views');
+const partialPath = path.join(__dirname,'../templates/partials');
 const app = express();
 const port = process.env.PORT || 3000;
 const {getNearbyX} = require('./datasrc/covidinfo');
@@ -12,7 +13,8 @@ app.use(express.static(publicDirectoryPath)); //for serving up the static files 
 
 //setup views and view engine as handlebars
 app.set('view engine', 'hbs');
-app.set('views', viewPath);
+app.set('views',viewPath);
+hbs.registerPartials(partialPath);
 
 //ROOT route
 app.get('/',(req,res)=>{
